c63bd8d487a65e767d961fa49a2b5a6b99a645bc
Finney Solomon <finney.solom.1@gmail.comgit config --global user.email finney.solomon.1@gmail.com>
2023-04-24 03:15:03 -0400

    added images

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,24 +1,63 @@
-import { AppBar, Button, Toolbar, Typography } from '@mui/material'
-import React from 'react'
-
+import {
+  AppBar,
+  Button,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import logo from "../Images/HomePageImages/Logo.png";
+import SchoolName from "../Images/HomePageImages/SchoolName.png";
 const Navbar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <AppBar position="static">
-        
-    <Toolbar>
-      <Typography variant="h6">
-        My App
-      </Typography>
-      <Button color="inherit">About Us</Button>
-      <Button color="inherit">Contact Us</Button>
-    </Toolbar>
-  </AppBar>
-  )
-}
+      <Toolbar>
+        <img
+          src={logo}
+          alt="Logo"
+          style={{ marginRight: "10px", height: "50px" }}
+        />
 
-export default Navbar
+        <img
+          src={SchoolName}
+          alt="School Name"
+          style={{ height: "40px", width: "320px" }}
+        />
 
+        <Button
+          color="inherit"
+          onClick={handleMenuOpen}
+          style={{ marginLeft: "auto" }}
+        >
+          About Us
+        </Button>
+        <>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem onClick={handleMenuClose}>About Me</MenuItem>
+            <MenuItem onClick={handleMenuClose}>School</MenuItem>
+            <MenuItem onClick={handleMenuClose}>College</MenuItem>
+          </Menu>
+        </>
+        <Button color="inherit" style={{ marginLeft: "auto" }}>
+          Contact Us
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-
-
-
+export default Navbar;
